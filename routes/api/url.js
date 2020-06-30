4078e074e6be1ffa4529da98ecf97affb2154a46
Tausif ali <ajaxhis@tutanota.com>
2020-06-30 23:51:29 +0530

    added url shorten and redirect routes

---
+++ routes/api/url.js
@@ -0,0 +1,97 @@
+const express = require("express");
+const router = express.Router();
+
+const { Url, Stats } = require("../../dbtables");
+
+const db = require("../../dbfn.js");
+const { getResponseBody, success, failure } = require("../../helper");
+
+const config = require("../../config.json");
+const conf = config[config.env];
+
+const path = require("path");
+
+const makeShort = (id) => {
+  let num = +id;
+  if (num <= 0) {
+    return null;
+  }
+  const map = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let shortUrl = [];
+  while (num > 0) {
+    shortUrl.push(map[num % 62]);
+    num = num / 62;
+    num = num | 0;
+  }
+  return shortUrl.reverse().join("");
+};
+
+const getLong = (short) => {
+  id = 0;
+  for (i = 0; i < short.length; i++) {
+    let ch = short[i].charCodeAt(0);
+    if ("a" <= short[i] && short[i] <= "z")
+      id = id * 62 + ch - "a".charCodeAt(0);
+    if ("A" <= short[i] && short[i] <= "Z")
+      id = id * 62 + ch - "A".charCodeAt(0) + 26;
+    if ("0" <= short[i] && short[i] <= "9")
+      id = id * 62 + ch - "0".charCodeAt(0) + 52;
+  }
+  return id;
+};
+
+const shortener = async (req, res) => {
+  const data = req.body;
+  const record = await db.post(Url, data);
+  if (!record) {
+    const response = getResponseBody(
+      failure,
+      "SHORT_FAIL",
+      "Internal server error!",
+      500
+    );
+    return res.status(response.statusCode).json(response);
+  }
+  const shortUrl = makeShort(record.id);
+  if (!shortUrl) {
+    const response = getResponseBody(
+      failure,
+      "SHORT_FAIL",
+      "Failed to shorten given Url!"
+    );
+    return res.status(response.statusCode).json(response);
+  }
+  const actual_short_url = `${conf.siteUrl}/${shortUrl}`;
+  const response = getResponseBody(success, "SHORT_OK", {
+    short_url: actual_short_url,
+  });
+  return res.status(response.statusCode).json(response);
+};
+
+const expander = async (req, res) => {
+  const shortUrl = req.params.shortUrl;
+  const id = +getLong(shortUrl);
+  const record = await db.get(Url, id);
+  if (!record) {
+    const response = getResponseBody(
+      failure,
+      "LONG_FAIL",
+      "Failed to expand the short Url!"
+    );
+    return res.status(response.statusCode).json(response);
+  }
+  await db.post(Stats, { url_id: id });
+  await db.put(Url, id, { visits: record.visits + 1 });
+  return res.redirect(record.long_url);
+};
+
+const main = (req, res) => {
+  return res.render('main')
+  // return res.sendFile(path.join(__dirname, "public", "index.html"));
+};
+
+router.get("/", main);
+router.get("/:shortUrl", expander);
+router.post("/api/v1/shorten", shortener);
+
+module.exports = router;
